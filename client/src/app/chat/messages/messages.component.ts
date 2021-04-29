4e80ad94a2e8b4d6aeffd77b3e8604f86fcd5ed1
Marc <marc@saebaweb.com>
2021-04-29 11:32:58 +0200

    Add Auto scroll

--- client/src/app/chat/messages/messages.component.ts
+++ client/src/app/chat/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewChecked } from '@angular/core';
 import { SocketService } from "./../../services/socket.service";
 
 @Component({
@@ -6,11 +6,26 @@ import { SocketService } from "./../../services/socket.service";
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent implements OnInit, AfterViewChecked {
+
+  @ViewChild('scrollMe') public myScrollContainer: ElementRef;
 
   constructor(public socket: SocketService) { }
 
   ngOnInit(): void {
+    this.scrollToBottom();
   }
+
+  ngAfterViewChecked() {
+        this.scrollToBottom();
+    }
+
+  scrollToBottom(): void {
+        try {
+            this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+        } catch(err) {
+          console.log(err);
+        }
+    }
 
 }
