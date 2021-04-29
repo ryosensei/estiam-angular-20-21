473747fb0accbbcee64a1fa0bd6acd8920eda7c9
Marc <marc@saebaweb.com>
2021-04-29 14:21:30 +0200

    Add notations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule }           from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
+import { ReactiveFormsModule }from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthComponent } from './auth/auth.component';
+import { AppRoutingModule }   from './app-routing.module';
+import { AppComponent }       from './app.component';
+import { AuthComponent }      from './auth/auth.component';
 
-import { UserService } from "./services/user.service";
-import { SocketService } from "./services/socket.service";
-import { ChatComponent } from './chat/chat.component';
-import { UsersComponent } from './chat/users/users.component';
-import { MessagesComponent } from './chat/messages/messages.component';
+import { UserService }        from "./services/user.service";
+import { SocketService }      from "./services/socket.service";
+import { ChatComponent }      from './chat/chat.component';
+import { UsersComponent }     from './chat/users/users.component';
+import { MessagesComponent }  from './chat/messages/messages.component';
 
 @NgModule({
   declarations: [
